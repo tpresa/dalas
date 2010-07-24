c80bb07fa2300f710f0776c212fd598630392b19
Português <portugues@arena04.(none)>
2010-07-23 21:13:39 -0300

    SQLObject schema generation implemented, broken

--- lib/dalas/mail/schema.py
+++ lib/dalas/mail/schema.py
@@ -2,7 +2,58 @@ from sqlobject import *
 
 class Message(SQLObject):
 	subject = StringCol()
+	sender = StringCol()
+	start = DateTimeCol()
+	end = DateTimeCol()
+	average = DateTimeCol()
+	size = IntCol()
+	checkin = DateTimeCol()
+	done = BoolCol()	
 	
+
+class Recipient(SQLObject):
+	label = StringCol()
+	email_to = StringCol()
+	orig_to = StringCol()
+	relay_info = StringCol()
+	delays = StringCol()
+	dsn = StringCol()
+	status = StringCol()
+	queue = ForeignKey()
+	checkin = DateTimeCol()
+
+class Reject(SQLObject):
+	label = StringCol()
+	ident = RelatedJoin('MsgId')
+	checkin = DateTimeCol()
+
+class MsgId(SQLObject):
+	label = StringCol()
+	msgid = StringCol()
+	msg = ForeignKey()
+	queue = RelatedJoin('Queue')
+	checkin = DateTimeCol()
+
+class Event(SQLObject):
+	label = StringCol()
+	raw_month = StringCol()
+	raw_day = StringCol()
+	raw_time = StringCol()
+	raw_hostname = StringCol()
+	raw_actor = StringCol()
+	raw_actor_cmd = StringCol()
+	raw_pid = StringCol()
+	raw_line = StringCol()
+	queue = RelatedJoin('Queue')
+	checkin = DateTimeCol()
+
+class Queue(SQLObject):
+	ident = StringCol()
+	label = StringCol()
+	done = BoolCol()
+
 def load_schema(connect):
-	Message._connection = connect
-	Message.createTable(ifNotExists=True)
+	tables = [Message]
+	for table in tables:
+		table._connection = connect
+		table.createTable(ifNotExists=True)
